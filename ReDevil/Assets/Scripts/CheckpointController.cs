f8437345ca048602c2666ccb56ddc4a6a825d158
Raquel <raqueldacosta602@gmail.com>
2021-05-08 09:54:07 -0400

    Checkpoint - Detects when player has triggered. Image changes

---
+++ ReDevil/Assets/Scripts/CheckpointController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointController : MonoBehaviour
+{
+    public Sprite cpNotTriggered;
+    public Sprite cpTriggered;
+    private SpriteRenderer cpSpriteRenderer;
+    public bool cpReached;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cpSpriteRenderer = GetComponent<SpriteRenderer>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            cpSpriteRenderer.sprite = cpTriggered;
+            cpReached = true;
+        }
+    }
+}
