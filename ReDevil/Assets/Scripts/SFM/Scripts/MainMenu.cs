d5937071b89af3df33b81e1e2057660371578198
Aidan <73758104+aidan-navarro@users.noreply.github.com>
2021-06-22 20:33:08 -0400

    UPDATE - Colors on Buttons
    Updated colors on buttons

--- ReDevil/Assets/Scripts/SFM/Scripts/MainMenu.cs
+++ ReDevil/Assets/Scripts/SFM/Scripts/MainMenu.cs
@@ -3,31 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using System;
 
-
+//This script holds all functions that are used on the main menu buttons
 public class MainMenu : MonoBehaviour
 {
-
-    public GameObject playButton, creditsButton, quitButton;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //if (Input.GetButtonDown("Submit"))
-        //{
-        //    EventSystem.current.SetSelectedGameObject(null);
-        //    EventSystem.current.SetSelectedGameObject(playButton);
-        //    PlayOption();
-        //    CreditsOption();
-        //    QuitOption();
-        //}
-    }
     public void PlayOption()
     {
         SceneManager.LoadScene("SampleScene");
@@ -43,6 +24,6 @@ public class MainMenu : MonoBehaviour
     {
         Debug.Log("Quitting Game");
         Application.Quit();
-        
+
     }
 }
