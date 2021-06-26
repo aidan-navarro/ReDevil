de56732f3ff7916aa0be746195372d7175b96c88
Vincent329 <vincee.ho@gmail.com>
2021-06-26 13:37:14 -0400

    [ADD] - Ground Attack Direction change mid combo.
    between attack transitions, if you move your analog stick either to the opposite direction and attack, the player will switch attack directions.

--- ReDevil/Assets/Scripts/SFM/Scripts/PlayerFSMStates/GroundAttack2State.cs
+++ ReDevil/Assets/Scripts/SFM/Scripts/PlayerFSMStates/GroundAttack2State.cs
@@ -66,6 +66,22 @@ public class GroundAttack2State : FSMState
         {
             attackStarted = false;
             patk.ReInitializeTransitions();
+            if (pc.moveVector.x > 0f)
+            {
+                pc.direction = 1;
+                pc.facingLeft = false;
+
+
+                pc.FlipPlayer();
+            }
+            else if (pc.moveVector.x < 0f)
+            {
+                pc.direction = -1;
+                pc.facingLeft = true;
+
+                pc.FlipPlayer();
+
+            }
             pc.PerformTransition(Transition.GroundAttack3);
         }
 
