6d49ee0d38c0c30204135bc1adf0fa9a53a48832
Flavio Araujo Silva <silvaflavio267@gmail.com>
2021-08-16 16:50:23 -0400

    Added in the DC Effect

--- ReDevil/Assets/HitSprite.cs
+++ ReDevil/Assets/HitSprite.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 
 public class HitSprite : MonoBehaviour
 {
-    private void Start()
-    {
-        Destroy(gameObject, 2);
-    }
     public void DestroySelf()
     {
         Destroy(gameObject);
