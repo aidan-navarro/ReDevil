bc774d146a2adfc32463686d9c50bb74ac0c8eb4
Aidan <73758104+aidan-navarro@users.noreply.github.com>
2021-08-20 13:26:02 -0400

    Update - Pause Menu and Death Menu
    Updated pause and death menu

--- ReDevil/Assets/Scripts/SFM/Scripts/DeathScreen.cs
+++ ReDevil/Assets/Scripts/SFM/Scripts/DeathScreen.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
+using TMPro;
 
 public class DeathScreen : MonoBehaviour
 {
     // we're not doing anything with these
     public GameObject retryButton, restartButton, withdrawButton;
+
+    [SerializeField]
+    private TextMeshProUGUI text;
 
     private static RespawnManager respawn;
 
@@ -24,6 +28,22 @@ public class DeathScreen : MonoBehaviour
         EventSystem.current.SetSelectedGameObject(null);
         //set to the first button in the pause menu
         EventSystem.current.SetSelectedGameObject(retryButton);
+    }
+
+    private void Update()
+    {
+        if (EventSystem.current.currentSelectedGameObject == retryButton)
+        {
+            text.SetText("Retry the level from the last reached checkpoint");
+        }
+        else if (EventSystem.current.currentSelectedGameObject == restartButton)
+        {
+            text.SetText("Restart the level from the beginning");
+        }
+        else
+        {
+            text.SetText("Return to the main menu");
+        }
     }
 
     public void RetryOption()
